e590c9fb8084516771dd4fec6b94f35d34a4d5ba
Guilherme <guipanfiglio123@outlook.com>
2022-04-13 15:28:18 -0300

    Configure GlobalStyle and Themes

---
+++ styles/GlobalStyle/index.jsx
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    border: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }  
+
+  body {
+    background: ${({ theme }) => theme.backgroundColor};
+    color: ${({ theme }) => theme.color};
+  }
+
+  ol, ul {
+    list-style: none;
+    padding-left: 0;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+export default GlobalStyle;
---
+++ styles/Themes/index.jsx
@@ -0,0 +1,9 @@
+export const Light = {
+  backgroundColor: "#f6f8fa",
+  color: "#212529"
+};
+
+export const Dark = {
+  backgroundColor: "#24292e",
+  color: "rgba(255, 255, 255, 0.87)",
+};

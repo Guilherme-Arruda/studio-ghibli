3a00f07d134da89e79f0a12a5d06317502c40791
Guilherme <guipanfiglio123@outlook.com>
2022-04-14 00:36:23 -0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,14 @@
+import AppProvider from '../context/AppContext';
+import GlobalStyle from '../styles/GlobalStyle';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <AppProvider>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </AppProvider>
+  );
 };
 
 export default MyApp;

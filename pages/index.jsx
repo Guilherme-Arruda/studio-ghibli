d81c93da4467b93ac71d8d55c1eed2b578db82ad
Guilherme <guipanfiglio123@outlook.com>
2022-04-14 00:35:57 -0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,38 @@
-const Home = () => {
-  return <h1>Olá, Mundo.</h1>;
+import Head from 'next/head';
+
+import NavBar from '../components/Navbar';
+import Container from '../components/Container';
+import MovieList from '../components/MovieList';
+import Movie from '../components/Movie';
+
+export async function getStaticProps() {
+  const response = await fetch('https://ghibliapi.herokuapp.com/films');
+  const data = await response.json();
+
+  return {
+    props: {
+      movies: data,
+    },
+  };
+}
+
+const Home = ({ movies }) => {
+  return (
+    <>
+      <Head>
+        <title>Studio Ghibli</title>
+      </Head>
+      <NavBar />
+      <Container>
+        <h1>A List of All the Studio Ghibli Movies</h1>
+        <MovieList>
+          {movies.map((movie) => {
+            return <Movie key={movie.id} src={movie.image} title={movie.title} id={movie.id} />;
+          })}
+        </MovieList>
+      </Container>
+    </>
+  );
 };
 
 export default Home;
